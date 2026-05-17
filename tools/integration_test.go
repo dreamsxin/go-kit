5a466f0a1a9fabab6ed757c68ddb95334374cbc2
dreamsxin <dreamsxin@qq.com>
2026-05-17 13:00:59 +0800

    test: split microgen idl generation smoke

--- tools/integration_test.go
+++ tools/integration_test.go
@@ -574,44 +574,6 @@ func TestMicrogenIntegration(t *testing.T) {
 	root := filepath.Dir(cwd)
 	microgenPath := microgenMainPath(t)
 
-	t.Run("IDL_DefaultFlags", func(t *testing.T) {
-		outDir := filepath.Join(cwd, "testdata", "gen_idl_default_flags")
-		os.RemoveAll(outDir)
-
-		idlFile := filepath.Join(root, "cmd", "microgen", "parser", "testdata", "basic.go")
-		cmd := exec.Command("go", "run", microgenPath,
-			"-idl", idlFile,
-			"-out", outDir,
-			"-import", "example.com/gen_idl_default_flags",
-		)
-		if out, err := cmd.CombinedOutput(); err != nil {
-			t.Fatalf("microgen idl default-flags failed: %v\n%s", err, out)
-		}
-
-		mustExistFile(t, filepath.Join(outDir, "go.mod"))
-		mustExistFile(t, filepath.Join(outDir, "idl.go"))
-		mustExistFile(t, filepath.Join(outDir, "service", "userservice", "service.go"))
-		mustExistFile(t, filepath.Join(outDir, "endpoint", "userservice", "endpoints.go"))
-		mustExistFile(t, filepath.Join(outDir, "endpoint", "userservice", "generated_chain.go"))
-		mustExistFile(t, filepath.Join(outDir, "endpoint", "userservice", "custom_chain.go"))
-		mustExistFile(t, filepath.Join(outDir, "transport", "userservice", "transport_http.go"))
-		mustExistFile(t, filepath.Join(outDir, "client", "userservice", "demo.go"))
-		mustExistFile(t, filepath.Join(outDir, "sdk", "userservicesdk", "client.go"))
-		mustExistFile(t, filepath.Join(outDir, "config", "config.yaml"))
-		mustExistFile(t, filepath.Join(outDir, "config", "config.go"))
-		mustExistFile(t, filepath.Join(outDir, "config", "local.go"))
-		mustExistFile(t, filepath.Join(outDir, "config", "env.go"))
-		mustExistFile(t, filepath.Join(outDir, "config", "remote.go"))
-		mustExistFile(t, filepath.Join(outDir, "config", "loader.go"))
-		mustExistFile(t, filepath.Join(outDir, "README.md"))
-		mustExistFile(t, filepath.Join(outDir, "model", "generated_user.go"))
-		mustExistFile(t, filepath.Join(outDir, "repository", "generated_user_repository.go"))
-		mustExistFile(t, filepath.Join(outDir, "skill", "skill.go"))
-		mustNotExistFile(t, filepath.Join(outDir, "docs", "docs.go"))
-		mustNotExistFile(t, filepath.Join(outDir, "transport", "userservice", "transport_grpc.go"))
-		mustNotExistFile(t, filepath.Join(outDir, "pb", "userservice", "userservice.proto"))
-	})
-
 	t.Run("IDL_Config_RemoteConsul_UsesRemoteAndFallsBackToLocal", func(t *testing.T) {
 		outDir := filepath.Join(cwd, "testdata", "gen_idl_remote_config")
 		os.RemoveAll(outDir)
---
+++ tools/microgen_idl_generation_test.go
@@ -0,0 +1,51 @@
+package tools_test
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+)
+
+func TestMicrogenIDLDefaultFlags(t *testing.T) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("Getwd: %v", err)
+	}
+	root := filepath.Dir(cwd)
+	outDir := filepath.Join(cwd, "testdata", "gen_idl_default_flags")
+	os.RemoveAll(outDir)
+
+	idlFile := filepath.Join(root, "cmd", "microgen", "parser", "testdata", "basic.go")
+	cmd := exec.Command("go", "run", microgenMainPath(t),
+		"-idl", idlFile,
+		"-out", outDir,
+		"-import", "example.com/gen_idl_default_flags",
+	)
+	if out, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("microgen idl default-flags failed: %v\n%s", err, out)
+	}
+
+	mustExistFile(t, filepath.Join(outDir, "go.mod"))
+	mustExistFile(t, filepath.Join(outDir, "idl.go"))
+	mustExistFile(t, filepath.Join(outDir, "service", "userservice", "service.go"))
+	mustExistFile(t, filepath.Join(outDir, "endpoint", "userservice", "endpoints.go"))
+	mustExistFile(t, filepath.Join(outDir, "endpoint", "userservice", "generated_chain.go"))
+	mustExistFile(t, filepath.Join(outDir, "endpoint", "userservice", "custom_chain.go"))
+	mustExistFile(t, filepath.Join(outDir, "transport", "userservice", "transport_http.go"))
+	mustExistFile(t, filepath.Join(outDir, "client", "userservice", "demo.go"))
+	mustExistFile(t, filepath.Join(outDir, "sdk", "userservicesdk", "client.go"))
+	mustExistFile(t, filepath.Join(outDir, "config", "config.yaml"))
+	mustExistFile(t, filepath.Join(outDir, "config", "config.go"))
+	mustExistFile(t, filepath.Join(outDir, "config", "local.go"))
+	mustExistFile(t, filepath.Join(outDir, "config", "env.go"))
+	mustExistFile(t, filepath.Join(outDir, "config", "remote.go"))
+	mustExistFile(t, filepath.Join(outDir, "config", "loader.go"))
+	mustExistFile(t, filepath.Join(outDir, "README.md"))
+	mustExistFile(t, filepath.Join(outDir, "model", "generated_user.go"))
+	mustExistFile(t, filepath.Join(outDir, "repository", "generated_user_repository.go"))
+	mustExistFile(t, filepath.Join(outDir, "skill", "skill.go"))
+	mustNotExistFile(t, filepath.Join(outDir, "docs", "docs.go"))
+	mustNotExistFile(t, filepath.Join(outDir, "transport", "userservice", "transport_grpc.go"))
+	mustNotExistFile(t, filepath.Join(outDir, "pb", "userservice", "userservice.proto"))
+}
